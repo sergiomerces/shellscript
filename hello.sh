d5b9230cb64d4c970d7e5cd0bb786df09cf35f90
Sergio Merces <sergio.merces@gmail.com>
2020-06-15 00:14:51 -0300

    Uso de variáveis

--- hello.sh
+++ hello.sh
@@ -1,4 +1,9 @@
+# Imprimir Hello World na tela
+
+# linha de comando para que o terminal interprete o código shell
 #!/usr/bin/env bash
 
 clear
-echo -e "\n\t\t\t\tHello World!\n\n\t\tBem-vindo à programação em shellscript!\n\n"
+
+# (echo -e) interpreta  os caracteres de escape \n \t
+echo -e "\n\t\t\t\tHello World!\n\n\t\tBem-vindo à programação em shellscript!\n\n\a"
--- primeiro.sh
+++ primeiro.sh
@@ -1,6 +1,9 @@
+# ir para a raiz, verifiar diretório e listar com detalhes
+
 #!/usr/bin/env bash
 
 cd /
 pwd
 ls -l
 
+# o arquivo shell pode ser salvo sem nenhuma extensão ou com a extensão .sh
